f6a1ab0d578f695ee73f2718eca14c0b4bc2b23c
Gage <60677479+MalachiSanderson@users.noreply.github.com>
2022-12-03 13:10:55 -0500

    var setup

--- TS_Decoder.hpp
+++ TS_Decoder.hpp
@@ -8,11 +8,24 @@
 #include <sstream>
 #include <string>
 
+unsigned char memory[65536];
 
+//Program counter (16 bit) used to hold the address of the next instruction to execute. It is initialized to zero.
+uint16_t PC = 0;
+
+//Instruction register (8 bit) used to hold the current instruction being executed
+uint8_t IR = 0;
+
+//Memory Address Register (16 bit) used to hold an address being used as a pointer, i.e., an indirect reference to data in memory
+uint16_t MAR = 0;
+
+//Accumulator (8 bit) used to operate on data
+uint8_t ACC = 0;
 
 void fetchNextInstruction(void);
 
 
 void executeInstruction(void);
+
 
 
