dca088b567af3a33003e450c5ad280aa811f699d
Gage <60677479+MalachiSanderson@users.noreply.github.com>
2022-12-03 12:41:45 -0500

    Pushed initial docs

---
+++ TS_Decoder.hpp
@@ -0,0 +1,18 @@
+
+#include <bitset>
+#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <string>
+
+
+
+void fetchNextInstruction(void);
+
+
+void executeInstruction(void);
+
+
